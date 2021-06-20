ad4fe76875bc7a93fbe1050f5b8c4a5e29453ab1
cskadir <kadirkorkmaz96@gmail.com>
2021-06-20 18:31:13 +0300

    HES ADDED

---
+++ src/egovernment/model/HES.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package egovernment.model;
+
+import egovernment.interfaces.Service;
+import java.util.Date;
+
+/**
+ *
+ * @author kadir
+ */
+public class HES  implements Service{
+    private String code;
+    private String identityNo;
+    private String explanation;
+    private String userName;
+    private String qrCode;
+    private Date date;
+
+    @Override
+    public void create(String identity, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void show() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
